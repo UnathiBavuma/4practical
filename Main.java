0908cd247b5727380fa54b83facd71f033d6d0da
UnathiBavuma <4573818@myuwc.ac.za>
2026-03-04 12:20:32 +0000

    i am continuing my code by adding a list to an array and start collecting shuffle list

--- Main.java
+++ Main.java
@@ -8,3 +8,15 @@ public class Main {
         String[] keys = new String[N];
         for (int i = 0; i < N; i++)
             keys[i] = String.valueOf(i + 1);
+            List<String> list = Arrays.asList(keys);
+        Collections.shuffle(list);
+
+        int[] loads = {75, 80, 85, 90, 95};
+
+        System.out.println("Load\tOpen(s)\tChained(s)");
+
+        for (int load : loads) {
+            int used = (int)(950000 * (load / 100.0));
+            int m = used;
+
+            double openTime = 0, chainTime = 0;
