4d8c0d2f998cb13838063ce9553872006f354e05
UnathiBavuma <4573818@myuwc.ac.za>
2026-03-04 13:41:40 +0000

    i just decided to re do the chained list thingy

--- ChainedHash.java
+++ ChainedHash.java
@@ -1,18 +1,13 @@
 import java.util.LinkedList;
 
 public class ChainedHash {
+
     static class Entry {
-        String key, value;
-        Entry(String k, String v) { key = k; value = v; }
-    }
+        String key;
+        String value;
 
-    private LinkedList<Entry>[] table;
-    private int m;
-
-    public ChainedHash(int m) {
-        this.m = m;
-        table = new LinkedList[m + 1];
-        for (int i = 1; i <= m; i++)
-            table[i] = new LinkedList<>();
+        Entry(String k, String v) {
+            key = k;
+            value = v;
+        }
     }
-    
